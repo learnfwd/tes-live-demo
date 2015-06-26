14f7dc72be763a26048e2b948d038a0b0e11aa46
Cristian Dinu <cristian.dinu@learnfwd.com>
2015-06-27 00:08:11 +0100

    Remove welcome bug, add exercise to control class

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 (function(window, document) {
   'use strict';
   
-  var App = require('lfa-core/app');
+  var App   = require('lfa-core/app');
+  var React = require('react'); 
   var $ = require('bootstrap');
+
   require('jqmath');
+
   var FakeNameGenerator = require('./fake-name-generator');
   var ClassMateList = require('lfa-classroom/views/classmate-list');
   var NameInput = require('lfa-classroom/views/name-input');
@@ -21,11 +24,6 @@
       $('#textbook').addClass('lfdemo-slide-container');
     }
 
-    setTimeout(function() {
-        App.trigger('avatar:say', 'Welcome!')
-      },
-      4000);
-
     if (studentList) {
       React.render(
         React.createElement(ClassMateList, null), studentList
@@ -37,9 +35,15 @@
         React.createElement(NameInput, null), nameInput
       );
     }
-
   });
 
+  setTimeout(function() {
+      App.trigger('avatar:say', 'Welcome!');
+    },
+    4000);
 
-
+  setTimeout(function() {
+      App.trigger('avatar:say', 'Vote for Learn Forward! :)');
+    },
+    240000);  
 })(this, document);
